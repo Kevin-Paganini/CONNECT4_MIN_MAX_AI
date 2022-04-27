b7231c5111b510725346c54c0de23c0df02b0c68
Aaron <closea@msoe.edu>
2022-04-27 16:49:52 -0500

    added place peice

--- src/main.py
+++ src/main.py
@@ -4,6 +4,13 @@ def main():
     board = Connect4()
     board.print_board()
 
+    board.place_piece(1, 1)
+    board.place_piece(1, 2)
+    board.place_piece(1, 1)
+    board.place_piece(1, 2)
+    board.place_piece(1, 1)
+    board.print_board()
+
 
 if __name__ == '__main__':
     main()
