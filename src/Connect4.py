bdac17d77b60a5aa3de7afb2c01c5e3823b61dd6
paganinik <paganinik@msoe.edu>
2022-04-27 16:50:41 -0500

    hi

--- src/Connect4.py
+++ src/Connect4.py
@@ -3,7 +3,26 @@ import numpy as np
 class Connect4():
 
     def __init__(self):
+        #player 1 is 1
+        #player 2 is 2
+        # 0 is empty space
         self._board = np.zeros((6, 7))
+        self.check_winner(1)
+        # []
+
 
     def print_board(self):
         print(self._board)
+
+
+
+
+    
+
+
+    def check_winner(self, player):
+        for idx, pos in np.ndenumerate(self._board):
+            print(f'Index: {idx}, value: {pos}')
+                
+        return False
+
